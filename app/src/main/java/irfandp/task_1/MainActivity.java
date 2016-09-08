e0ab2f931a223eb30b42380369dd8095a51a7d1d
irfand19 <irfand19@gmail.com>
2016-09-08 10:42:57 +0700

    default keyboard sudah off, inputan untuk income dan outcome sudah bisa

--- app/src/main/java/irfandp/task_1/MainActivity.java
+++ app/src/main/java/irfandp/task_1/MainActivity.java
@@ -2,12 +2,230 @@ package irfandp.task_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button_clr, button_del;
+    EditText edt1, edt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button_clr = (Button) findViewById(R.id.button_clr);
+        button_del = (Button) findViewById(R.id.button_del);
+        edt1 = (EditText) findViewById(R.id.edt1);
+        edt2 = (EditText) findViewById(R.id.edt2);
+
+        edt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "1");
+                    }
+                });
+
+                button2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "2");
+                    }
+                });
+
+                button3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "3");
+                    }
+                });
+
+                button4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "4");
+                    }
+                });
+
+                button5.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "5");
+                    }
+                });
+
+                button6.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "6");
+                    }
+                });
+
+                button7.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "7");
+                    }
+                });
+
+                button8.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "8");
+                    }
+                });
+
+                button9.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "9");
+                    }
+                });
+
+                button0.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText(edt1.getText() + "0");
+                    }
+                });
+
+                button_clr.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt1.setText("");
+                    }
+                });
+
+                button_del.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String str = edt1.getText().toString();
+                        if (str.length() >= 1) {
+                            str = str.substring(0, str.length() - 1);
+                            edt1.setText(str);
+                        } else if (str.length() <= 1) {
+                            edt1.setText("0");
+                        }
+                    }
+                });
+
+            }
+        });
+
+        edt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "1");
+                    }
+                });
+
+                button2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "2");
+                    }
+                });
+
+                button3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "3");
+                    }
+                });
+
+                button4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "4");
+                    }
+                });
+
+                button5.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "5");
+                    }
+                });
+
+                button6.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "6");
+                    }
+                });
+
+                button7.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "7");
+                    }
+                });
+
+                button8.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "8");
+                    }
+                });
+
+                button9.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "9");
+                    }
+                });
+
+                button0.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText(edt2.getText() + "0");
+                    }
+                });
+
+                button_clr.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edt2.setText("");
+                    }
+                });
+
+                button_del.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String str = edt2.getText().toString();
+                        if (str.length() >= 1) {
+                            str = str.substring(0, str.length() - 1);
+                            edt2.setText(str);
+                        } else if (str.length() <= 1) {
+                            edt2.setText("0");
+                        }
+                    }
+                });
+
+            }
+        });
+
+
     }
 }
+
