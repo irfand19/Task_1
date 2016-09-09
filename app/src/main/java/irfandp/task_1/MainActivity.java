2f6b2536538dce2e420e96ac9c043ad5f8767710
irfand19 <irfand19@gmail.com>
2016-09-09 14:07:26 +0700

    tambah background

--- app/src/main/java/irfandp/task_1/MainActivity.java
+++ app/src/main/java/irfandp/task_1/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TabWidget;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
